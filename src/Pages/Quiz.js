2ecf1f8dfa1451e3df1a0ecb86bd9d112adff4a2
RThemian <62709684+RThemian@users.noreply.github.com>
2022-07-23 20:14:08 -0600

    update with questions posted

--- src/Pages/Quiz.js
+++ src/Pages/Quiz.js
@@ -1,13 +1,52 @@
-import React from 'react'
+import React, {useState} from 'react';
 
-import {useNavigate} from 'react-router-dom';
+import axios from 'axios';
 
 function Quiz() {
+
+    const difficultyLevel = ['easy', 'medium', 'hard'];
+
+    const [score, setScore] = useState(0);
+
+
+
+    const [questions, setQuestions] = useState([
+    ])
+
+    
+
+    const url = 'https://opentdb.com/api.php?amount=10&difficulty=easy&type=multiple'
+
+    const loadQuestions = () => {
+        
+    return axios.get(url)
+        .then( (response) => {
+    // handle success
+         console.log("questions?", response.data.results);
+        setQuestions(response.data.results)
+        })
+        .catch((error) => {
+    // handle error
+            console.log(error);
+        })
+        .then( () => {
+    // always executed
+         console.log("axios executed!")
+  });
+
+    }
+
   return (
+      <>
     <div className = 'container'>
         <h3>QUIZ</h3>
-        <button>Change to about page</button>
+        <button onClick = {loadQuestions} >Load Questions? Easy level</button>
     </div>
+    <div className='questions'>
+        {questions[0] ? questions.map(question => <h2>{question.question}</h2>) : "" }
+
+    </div>
+    </>
   )
 }
 
