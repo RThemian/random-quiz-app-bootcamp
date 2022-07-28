facccb2d70d611db678aabdab5a48ea46f32233c
Tomas Paul Programmer <62709684+RThemian@users.noreply.github.com>
2022-07-28 14:02:28 -0600

    initial commit

--- src/Pages/Quiz.js
+++ src/Pages/Quiz.js
@@ -1,88 +1,83 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
-import axios from 'axios';
+import axios from "axios";
 
 const Quiz = () => {
+  const difficultyLevel = ["easy", "medium", "hard"];
 
-    const difficultyLevel = ['easy', 'medium', 'hard'];
+  const [score, setScore] = useState(0);
 
-    const [score, setScore] = useState(0);
+  const [optionChosen, setOptionChosen] = useState("");
 
-    const [optionChosen, setOptionChosen] = useState("");
+  const [questions, setQuestions] = useState([]);
 
-    const [questions, setQuestions] = useState([
-    ])
+  // function regexRemover find "&quot;" and replace with " " "
+  // find "&#039;" replace " ' "
 
-    // function regexRemover find "&quot;" and replace with " " "
-    // find "&#039;" replace " ' "
+  const removeSpecChar = (props) => {
+    let result = props
+      .replace(/&quot;/g, "''")
+      .replace(/&#039;/g, "'")
+      .replace(/&shy;/g, "-")
+      .replace(/&amp;/g, "&");
 
-    const removeSpecChar = (props) => {
+    return result;
+  };
 
-       let result = props
-         .replace(/&quot;/g, "''")
-         .replace(/&#039;/g, "'")
-         .replace(/&shy;/g, "-")
-         .replace(/&amp;/g, "&")
+  const randArray = () => {};
 
-        return result;
+  const url =
+    "https://opentdb.com/api.php?amount=10&difficulty=easy&type=multiple";
 
-    }
-
-    
-    const randArray = () => {
-
-    }
-
-
-
-    const url = 'https://opentdb.com/api.php?amount=10&difficulty=easy&type=multiple'
-
-    const loadQuestions = () => {
-        
-    return axios.get(url)
-        .then( (response) => {
-    // handle success
+  const loadQuestions = () => {
+    return axios
+      .get(url)
+      .then((response) => {
+        // handle success
         //response.json()
         console.log("questions?", response.data.results);
-        
-        setQuestions(response.data.results)
-        })
-        .catch((error) => {
-    // handle error
-            console.log(error);
-        })
-        .then( () => {
-    // always executed
-         console.log("axios executed!")
-  });
 
-    }
+        setQuestions(response.data.results);
+      })
+      .catch((error) => {
+        // handle error
+        console.log(error);
+      })
+      .then(() => {
+        // always executed
+        console.log("axios executed!");
+      });
+  };
 
   return (
-      <>
-    <div className = 'container'>
+    <>
+      <div className="container">
         <h3>QUIZ</h3>
-        <button onClick = {loadQuestions} >Load Questions? Easy level</button>
-    </div>
-    <div className='questions'>
-        {questions[0] ? questions.map(question => (
-        <ol key = {question.question}>
-            <h2>{removeSpecChar(question.question)}</h2>
-            <button id = "btn" onClick ={(event) => 
-          setOptionChosen("A")}> 
-          {removeSpecChar(question.incorrect_answers[0])}</button>{" "}
-        <button id = "btn" onClick ={() => setOptionChosen("B")}> 
-          {removeSpecChar(question.incorrect_answers[1])}</button>{" "}
-        <button id = "btn" onClick ={() => setOptionChosen("C")}> 
-          {removeSpecChar(question.incorrect_answers[2])}{" "} </button>{" "}
-        <button id = "btn" onClick ={() => setOptionChosen("D")}> 
-          {removeSpecChar(question.correct_answer)}{" "} </button>
-        </ol>
-        )) : "" }
-
-    </div>
+        <button onClick={loadQuestions}>Load Questions? Easy level</button>
+      </div>
+      <div className="questions">
+        {questions
+          ? questions.map((question) => (
+              <ol key={question.question}>
+                <h2>{removeSpecChar(question.question)}</h2>
+                <button id="btn" onClick={(event) => setOptionChosen("A")}>
+                  {removeSpecChar(question.incorrect_answers[0])}
+                </button>{" "}
+                <button id="btn" onClick={() => setOptionChosen("B")}>
+                  {removeSpecChar(question.incorrect_answers[1])}
+                </button>{" "}
+                <button id="btn" onClick={() => setOptionChosen("C")}>
+                  {removeSpecChar(question.incorrect_answers[2])}{" "}
+                </button>{" "}
+                <button id="btn" onClick={() => setOptionChosen("D")}>
+                  {removeSpecChar(question.correct_answer)}{" "}
+                </button>
+              </ol>
+            ))
+          : ""}
+      </div>
     </>
-  )
-}
+  );
+};
 
 export default Quiz;
