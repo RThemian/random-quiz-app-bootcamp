0f894dc2c9fab3cf68e0d5450a086f78bd5664bb
Cservenak <62709684+RThemian@users.noreply.github.com>
2022-08-11 08:44:50 -0600

    update button

--- src/Pages/StartScreen.js
+++ src/Pages/StartScreen.js
@@ -1,17 +1,20 @@
 import React from "react";
+import Button from "mui-button";
 import { Link } from "react-router-dom";
 
 function StartScreen() {
   return (
     <>
       <div>
-        This is the StartScreen,{" "}
+        QUIZ MASTER!,{" "}
         <span style={{ color: "red" }}>want to play a game?</span>
       </div>
 
       <label>PRESS THE BUTTON</label>
       <br />
+      <Button color="error" variant="outlined">
       <Link to="/quiz"><h1>Quiz</h1></Link>
+    </Button>
     </>
   );
 }
