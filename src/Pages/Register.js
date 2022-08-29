e7360d0d3b216f1479da8fc4db26bcf558faa7d1
RThemian <62709684+RThemian@users.noreply.github.com>
2022-08-29 13:18:40 -0600

    register works

--- src/Pages/Register.js
+++ src/Pages/Register.js
@@ -1,18 +1,63 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  useNavigate,
+} from "react-router-dom";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { auth, ColRef, Database } from "../Context/firebase";
 
+//enable registration using auth
 
 const Register = () => {
-
-    const [registerPassword, setRegisterPassword] = useState("");
+  const [registerPassword, setRegisterPassword] = useState("");
   const [registerEmail, setRegisterEmail] = useState("");
-
-  const [loginEmail, setLoginEmail] = useState("");
-
-  const [loginPassword, setLoginPassword] = useState("");
   const [user, setUser] = useState({});
-  return (
-    <div><h1>Register YOUR EMAIL</h1></div>
-  )
-}
 
-export default Register
+  const register = async () => {
+    try {
+      const user = await createUserWithEmailAndPassword(
+        auth,
+        registerEmail,
+        registerPassword
+      );
+      setRegisterEmail("");
+      setRegisterPassword("");
+      console.log(user);
+    } catch (error) {
+      //invalid email
+      console.log(error.message);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Register YOUR EMAIL</h1>
+      <div>
+        <h3>Register User</h3>
+        <input
+          placeholder="Email..."
+          value={registerEmail}
+          onChange={(event) => setRegisterEmail(event.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password..."
+          value={registerPassword}
+          onChange={(event) => setRegisterPassword(event.target.value)}
+        />
+        <button onClick={register}>Create User</button>
+      </div>
+      {user.email && <h2> New user created: {user.email}</h2>}
+    </div>
+  );
+};
+
+export default Register;
